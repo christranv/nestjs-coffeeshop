81119529e7a339adf65378e250deeef37af7011c
Chris Tran <chuong.tran@email.com>
2023-08-01 09:57:57 +0700

    Update README

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 <h1> Coffee Shop API </h1>
 
 ![Status](https://img.shields.io/badge/Status-Developing-0287D0?style=flat-square)&nbsp;
-![NestJS](https://img.shields.io/badge/Terraform-1%2E0%2E0-E0234E?style=flat-square&logo=nestjs&logoColor=E0234E)&nbsp;
+![NestJS](https://img.shields.io/badge/NestJS-10%2E0-E0234E?style=flat-square&logo=nestjs&logoColor=E0234E)&nbsp;
 ![Postgres](https://img.shields.io/badge/Posgres-15%2E3-4169E1?style=flat-square&logo=PostgreSQL&logoColor=white)&nbsp;
 ![nodejs](https://img.shields.io/badge/Node.js-18-3C873A?style=flat-square&logo=nodedotjs&logoColor=white)
 ![docker](https://img.shields.io/badge/Docker-latest-2CA5E0?style=flat-square&logo=docker&logoColor=white)&nbsp;
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api/api.module';
 // import * as compression from 'compression';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
@@ -10,6 +11,15 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<string>('http.port');
   if (!port) throw new Error('Missing PORT config');
+
+  const config = new DocumentBuilder()
+    .setTitle('nestjs-coffeeshop')
+    .setDescription('The Coffee Shop API document')
+    .setVersion('1.0')
+    .addTag('apis')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port);
 }
