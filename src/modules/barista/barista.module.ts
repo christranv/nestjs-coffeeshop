189c96034f4902c71aa50d3ff76782c606271b46
Chris Tran <chuong.tran@email.com>
2023-08-04 10:33:35 +0700

    Re-struct  folder

--- src/modules/barista/barista.module.ts
+++ src/modules/barista/barista.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SharedModule } from '@src/shared/shared.module';
-import { BaristaController } from './api/controllers/barista.controller';
+import { BaristaController } from './api/barista.controller';
 import { BaristaItem } from './domain/barista-item';
 
 @Module({
